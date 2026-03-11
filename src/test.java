6403d8c1f8c76f86ff48a80811f0ddee3ff0b9ad
cksam <cksamh@gmail.com>
2026-03-11 21:20:30 +1000

    added a for loop for practice

--- src/test.java
+++ src/test.java
@@ -1,6 +1,11 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+
+        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            j = j + i;
+        }
+        System.out.println(j);
     }
 }
